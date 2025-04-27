302bb75bd4800dab6a91ff6f99563fbb23d430c8
HowXu <howxu366@outlook.com>
2025-04-27 23:25:32 +0800

    ✨ feat(ArrayList): new data structure
    constructor, new func insert and resize

---
+++ src/Structure/LinearList/ArrayList.cs
@@ -0,0 +1,115 @@
+namespace Structure.LinearList;
+
+// 基于数组的线性表 称为ArrayList
+public class ArrayList<T>
+{
+    // 基于数组所以内部有一个数组 不可能为空
+    private T[]? inner_array;
+    // 容量声明
+    private int capacity;
+    private int default_capacity = 8;
+
+    // 数组元素声明
+    public int size = 0;
+
+    // 构造函数初始化
+    // 这个构造函数要分两类 一种传入数组给你造的 一种传入多参数给你造的
+    // 这个Array可以是可变参数 也可以直接数组
+    public ArrayList(int capacity = 8, params T[]? array)
+    {
+        // 首先应该判断array
+        if (array is not null && array.Length != 0)
+        {
+            // 当这个 array 不为空时 应该把内部数组初始化为array
+            this.capacity = array.Length;
+            size = array.Length;
+            inner_array = (T[])array.Clone(); // 浅拷贝
+        }
+        else
+        {
+            size = 0;
+            this.capacity = capacity; // 容量声明
+            inner_array = new T[capacity]; // 直接new一个就行
+        }
+    }
+    // 两种写法
+    public ArrayList(params T[]? array)
+    {
+        if (array is not null && array.Length != 0)
+        {
+            // 当这个 array 不为空时 应该把内部数组初始化为array
+            capacity = array.Length;
+            size = array.Length;
+            inner_array = (T[])array.Clone(); // 浅拷贝
+        }
+        else
+        {
+            size = 0;
+            capacity = 8; // 容量声明
+            inner_array = new T[capacity]; // 直接new一个就行
+        }
+    }
+
+    // 插入元素函数 参数为index和插入元素
+    public void Insert(int index,T element){
+        // 首先判断插入长度是否超过
+        if(index < 0 || index >= capacity){
+            // 不能在大于等于capacity的位置插入的 也不能在负数位置
+            throw new IndexOutOfRangeException();
+        }else{
+            // 插入前要判断一下是不是已经满了
+            if(capacity == size){
+                // 扩容 这个扩容的机制其实相当复杂(内存和运行 空间和时间) 这里简单一点就是加一倍的默认capacity
+                Resize(capacity + default_capacity);
+            }
+
+            // 执行插入逻辑 把已有的元素从index位置后移1位
+            // BUG 这个后面的怎么全变成NULL了
+            for(int i = size - 1; i >= index;i--){
+                // 因为前面进行扩容了 所以这里一定不会索引超出
+                inner_array![i+1] = inner_array[i]; // 一定不会为空
+            }
+            // 移动之后赋值就行了
+            inner_array![index] = element;
+            size += 1; // size变化
+        }
+    }
+
+    // 动态扩容函数
+    private void Resize(int needed_capacity){
+        // 这个Resize我们直接给到需求空间 最大化减少内存使用
+        // C# 没有足够强大的数组操作函数 所以只能比较低效地进行一下
+        T[] newArray = new T[needed_capacity];
+        // 复制新的数组
+        Array.Copy(inner_array!,newArray, capacity); // 这里inner_array 不可能为空 拷贝长度用capacity省一层访问开销
+        inner_array = newArray; // 重新设置指针 原来的inner_array会被回收
+        capacity = needed_capacity; // 容量变化
+    }
+
+    // 转换为字符串函数 这个不是普适的 Debug Only
+    public override string ToString()
+    {
+        if (size == 0)
+        {
+            return "[]";
+        }
+
+        var result = new System.Text.StringBuilder('[');
+        for (int i = 0; i < size; i++)
+        {
+            if(inner_array![i] != null){
+                result.Append(inner_array![i].ToString());
+            }else{
+                result.Append("null");
+            }
+            
+            if (i < size - 1)
+            {
+                result.Append(", ");
+            }
+        }
+        result.Append(']');
+        return result.ToString();
+    }
+
+}
--- src/Test/Program.cs
+++ src/Test/Program.cs
@@ -1,12 +1,37 @@
 namespace Test;
 
 using Algorithm;
-using Helper;
+using Structure.LinearList;
 
-class Program{
-    static void Main(String[] args){
-        int[] arr = new int[] { 123, 1, 23, 4, 5 };
+class Program
+{
+    static void Main(String[] args)
+    {
+        int[] arr = [123, 1, 23, 4, 5];
         Array.Sort(arr);
-        Console.WriteLine("二分查找法结果: {0}",A1BinarySearch.binarySearch(arr,0));
+        Console.WriteLine("二分查找法结果: {0}", A1BinarySearch.binarySearch(arr, 0));
+
+        Person[] persons = [new Person(10, "a"), new Person(12, "b"), new Person(13, "c")];
+        
+        ArrayList<Person> arrayList = new(3, persons); // 逆天写法
+        arrayList.Insert(0, new Person(8, "as"));
+        Console.WriteLine(arrayList.size);
+        Console.WriteLine(arrayList.ToString());
+    }
+
+    class Person
+    {
+        private int age { get; set; }
+        private String name { get; set; }
+
+        public Person(int age, String name)
+        {
+            this.age = age;
+            this.name = name;
+        }
+
+        public override String ToString(){
+            return name;
+        }
     }
 }
